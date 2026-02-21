d405b15c644236c45c752bbda8d59c6e6d3ae8f8
Pixel <pixel@webmail.com>
2026-02-22 01:56:50 +0300

    Lab4

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'libraryproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [BASE_DIR / "apps/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [( os.path.join(BASE_DIR, "apps/static"))]
